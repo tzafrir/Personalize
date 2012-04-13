3e6dd999528691741966a7b0e8fddf243e2c4ce7
Tzafrir Rehan <tzafrir.r@gmail.com>
2012-04-13 19:11:03 +0300

    Add new design button

--- personalize.js
+++ personalize.js
@@ -7,6 +7,51 @@ var CONTENT_PANE_ID = '#contentPane';
 var STREAM_UPDATE_SELECTOR = 'div[id^="update"]';
 var PUBLIC_POST_SELECTOR = '.ERIVId';
 
+var STATIC_CSS = "background-attachment: scroll;" +
+"background-clip: border-box;" +
+"background-color: " +
+"whiteSmoke;" +
+"background-image: -webkit-linear-gradient(top, " +
+"whiteSmoke, " +
+"#F1F1F1);" +
+"background-origin: padding-box;" +
+"border: 1px solid rgba(0, 0, 0, 0.0976563);" +
+"border-radius: 2px;" +
+"color: #444;" +
+"cursor: default;" +
+"display: inline-block;" +
+"font-family: arial, sans-serif;" +
+"font-size: 11px;" +
+"font-style: normal;" +
+"font-variant: normal;" +
+"font-weight: bold;" +
+"height: 27px;" +
+"line-height: 27px;" +
+"margin-bottom: 0px;" +
+"margin-left: 10px;" +
+"margin-right: 0px;" +
+"margin-top: 0px;" +
+"max-width: 120px;" +
+"min-width: 34px;" +
+"outline-color: #444;" +
+"outline-style: none;" +
+"outline-width: 0px;" +
+"overflow-x: hidden;" +
+"overflow-y: hidden;" +
+"padding-bottom: 0px;" +
+"padding-left: 21px;" +
+"padding-right: 12px;" +
+"padding-top: 0px;" +
+"position: relative;" +
+"text-align: center;" +
+"text-overflow: ellipsis;" +
+"vertical-align: middle;" +
+"visibility: visible;" +
+"white-space: nowrap;" +
+"z-index: 3;";
+
+var enabled = false;
+
 function onContentModified(e) {
   // This happens when a new stream is selected
   if (e.relatedNode && e.relatedNode.parentNode && e.relatedNode.parentNode.id == 'contentPane') {
@@ -15,7 +60,40 @@ function onContentModified(e) {
   } else if (e.target.nodeType == Node.ELEMENT_NODE && e.target.id.indexOf('update') == 0) {
     processPost(e.target);
   }
+  addPersonalizeButtonIfNeeded();
 };
+
+function addPersonalizeButtonIfNeeded() {
+  var firstButton = document.querySelector('[data-dest=stream]:first-child:not([tz_personalize])');
+  if (!firstButton) {
+    return;
+  }
+  if (document.getElementById('tz_personalizeButton')) {
+    return;
+  }
+  console.log(firstButton.getAttribute('tz_personalize'));
+  firstButton.setAttribute('tz_personalize', true);
+
+  var button = document.createElement('div');
+  button.id = 'tz_personalizeButton';
+  button.style.cssText = STATIC_CSS;
+
+  var controlBox = document.createElement('input');
+  controlBox.type = 'checkbox';
+  controlBox.onchange = onPersonalizeStateChange;
+  controlBox.id = 'tz_personalize_enable';
+  controlBox.style.cssText = 'position:absolute;top:3px;left:0px';
+  if (enabled) {
+    controlBox.setAttribute('checked', true);
+  }
+  var label = document.createElement('label');
+  label.setAttribute('for', controlBox.id);
+
+  button.appendChild(controlBox);
+  button.appendChild(document.createTextNode('Personalize'));
+  label.appendChild(button);
+  firstButton.parentElement.parentElement.appendChild(label);
+}
 
 /**
  * Process
@@ -31,7 +109,7 @@ function processAllItems(subtreeDOM) {
 
 function processPost(itemDOM) {
   if (itemDOM && itemDOM.querySelector(PUBLIC_POST_SELECTOR)) {
-    if (localStorage['enable'] == '1') {
+    if (enabled == '1') {
       itemDOM.style.visibility = 'hidden';
       itemDOM.style.display = 'none';
     } else {
@@ -41,33 +119,14 @@ function processPost(itemDOM) {
   }
 };
 
-function onChange(event) {
+function onPersonalizeStateChange(event) {
   var value = event.srcElement.checked;
-  localStorage['enable'] = value ? '1' : '0';
+  enabled = value ? '1' : '0';
   processAllItems();
   chrome.extension.sendRequest({personalize: value}, function(){});
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Insert checkbox.
-  var stream = document.querySelector('[asrc=streamnav]');
-  if (stream) {
-    var isEnabled = localStorage['enable'] == '1';
-    var controlBox = document.createElement('input');
-    controlBox.type = 'checkbox';
-    controlBox.onchange = onChange;
-    controlBox.id = 'tz_personalize_enable';
-    if (isEnabled) {
-      controlBox.setAttribute('checked', true);
-    }
-    var label = document.createElement('label');
-    label.setAttribute('for', controlBox.id);
-    label.innerText = 'Personalize';
-    var parent = stream.parentElement;
-    parent.insertBefore(label, parent.children[1]);
-    parent.insertBefore(controlBox, label);
-  }
-
   // Listen when the subtree is modified for new posts.
   var googlePlusContentPane = document.querySelector(CONTENT_PANE_ID);
   if (googlePlusContentPane) {
